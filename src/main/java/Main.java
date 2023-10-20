ff8ae6b5e69c376b47cdb33eed61911ee61ef15a
Guardian <antonzelenkov112@gmail.com>
2023-10-20 16:23:57 +0300

    0.0.7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ import java.lang.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Good day World - Задание 1"); //Задание 1
-        System.out.println("OS: " + System.getProperty("os.name") + "Задание с **"); //Задание **
+        System.out.println("OS: " + System.getProperty("os.name") + " - Задание с **"); //Задание **
     }
 }
